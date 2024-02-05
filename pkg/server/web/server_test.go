e47b4c0fdeecd31a5548d9b32c70725194c863c4
amir <mirzayi994@gmail.com>
2024-02-05 20:41:45 +0330

    refactor: unit test http server

--- pkg/server/web/server_test.go
+++ pkg/server/web/server_test.go
@@ -8,28 +8,36 @@ import (
 )
 
 func TestRunServer(t *testing.T) {
-	timeout := time.Second
-	mockedHttpServerAddress := "localhost:54321"
-	testingRoute := "/test"
-	fullTestingRoute := "http://" + mockedHttpServerAddress + testingRoute
+	const (
+		timeout                 = time.Second
+		mockedHttpServerAddress = "localhost:54321"
+		testingRoute            = "/test"
+		fullTestingRoute        = "http://" + mockedHttpServerAddress + testingRoute
+	)
 
 	srv := NewServer(mockedHttpServerAddress, nil, 100000, timeout, timeout, timeout, timeout)
-	go func() {
-		if err := srv.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-			t.Fail()
-		}
-	}()
 
-	srv.GetMuxHandler().HandleFunc(testingRoute, func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
+	t.Run("test run http server", func(t *testing.T) {
+		go func() {
+			if err := srv.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+				t.Fail()
+			}
+		}()
+		srv.GetMuxHandler().HandleFunc(testingRoute, func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		})
 	})
 
-	res, err := http.Get(fullTestingRoute)
-	if err != nil || res.StatusCode != http.StatusOK {
-		t.Fail()
-	}
+	t.Run("test http server functionality", func(t *testing.T) {
+		res, err := http.Get(fullTestingRoute)
+		if err != nil || res.StatusCode != http.StatusOK {
+			t.Fail()
+		}
+	})
 
-	if err := srv.GracefulShutdown(10 * time.Second); err != nil {
-		t.Fail()
-	}
+	t.Run("test shut down gracefully http server", func(t *testing.T) {
+		if err := srv.GracefulShutdown(10 * time.Second); err != nil {
+			t.Fail()
+		}
+	})
 }
